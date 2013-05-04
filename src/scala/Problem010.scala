2c5b9ad0b520bc603138583bd811c9ef7506fbd0
Nicolas Rinaudo <nicolas@nrinaudo.com>
2013-05-04 19:36:57 +0200

    Added solution to problem 10.

---
+++ src/scala/Problem010.scala
@@ -0,0 +1,11 @@
+/*
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * 
+ * Find the sum of all the primes below two million.
+ */
+import EulerLong._
+
+def sumPrimes(n: Int) = primes.takeWhile(_ < n).sum
+
+assert(sumPrimes(10) == 17)
+println(sumPrimes(2000000))
