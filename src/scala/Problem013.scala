65ed40ca1eb3b28a2a9645f823a86f03ab7c08de
Nicolas Rinaudo <nicolas@nrinaudo.com>
2013-05-06 10:57:22 +0200

    Now uses EulerTimer.

--- src/scala/Problem013.scala
+++ src/scala/Problem013.scala
@@ -255,5 +255,4 @@ object LongNumber {
   implicit def apply(value: String) = new LongNumber(value.toList.reverse.map {_ - 48})
 }
 
-// TODO: rework this so that truncate(10) can be used by EulerTimer.
-println(data.foldLeft(LongNumber("0")) {_ + _}.truncate(10))
+EulerTimer {data.foldLeft(LongNumber("0")) {_ + _}.truncate(10)}
