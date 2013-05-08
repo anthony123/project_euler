15e4beebb1fa7cd067c21cb0b3ab09fc4a5548fd
Nicolas Rinaudo <nicolas@nrinaudo.com>
2013-05-08 20:12:27 +0200

    Added solution to problem 20.

---
+++ src/scala/Problem020.scala
@@ -0,0 +1,15 @@
+/*
+ * n! means n x (n - 1) x ... x 3 x 2 x 1
+ * 
+ * For example, 10! = 10 x 9 x ... x 3 x 2 x 1 = 3628800,
+ * and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+ * 
+ * Find the sum of the digits in the number 100!
+ */
+
+def fact(n: Int, result: LongNumber = LongNumber(1)): LongNumber = {
+  if(n == 0) result
+  else fact(n - 1, result * n)
+}
+
+EulerTimer {fact(100).n.sum}
