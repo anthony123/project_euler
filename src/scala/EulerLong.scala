999b4fd3d78653d601a1c3d8e168f0f1bca0c586
Nicolas Rinaudo <nicolas@nrinaudo.com>
2013-05-11 19:52:44 +0200

    Prime factors calculation was moved to EulerLong.

--- src/scala/EulerLong.scala
+++ src/scala/EulerLong.scala
@@ -1,12 +1,23 @@
 /**
-  * Long utility class. In its current state, this is mostly used to factorize prime number manipulation.
+  * Utility class used to factorize recurrent number manipulation problems.
+  * Project Euler is particularly keen on prime numbers and prime factors, and this class handles them as fast as I
+  * know how. It's not terribly memory efficient, however.
   */
 class EulerLong(val n: Long) {
   import EulerLong._
 
   // Checks whether the current instance is a prime number.
-  def isPrime: Boolean = (n > 1) && (primes.takeWhile {_ <= scala.math.sqrt(n)}.forall {n % _ != 0})
+  def isPrime: Boolean = checkPrime(n)
 
+  // Returns the current instance's prime factors.
+  def primeFactors() = divisors.filter {checkPrime}
+
+  // Returns the current instance's divisors.
+  // The "clever" bit here is that there is no need to check for numbers larger than sqrt(n).
+  // If n = m * o:
+  // - m = o,  which means that sqrt(n) is a divisor.
+  // - m != o, in which case one is larger and the other smaller than sqrt(n). By checking until sqrt(n), we will
+  //           find all values of m such that n = m * o. For each m, we've also found o = n / m.
   def divisors =  {
     def divisorsR(m: Long, result: List[Long]): List[Long] = {
       if(m == 1) result
@@ -28,11 +39,16 @@ object EulerLong {
   implicit def longToEulerLong(value: Long) = new EulerLong(value)
   implicit def intToEulerLong(value: Int) = new EulerLong(value)
 
+  // Checks whether the specified long is a prime number.
+  private def checkPrime(n: Long): Boolean = (n > 1) && (primes.takeWhile {_ <= scala.math.sqrt(n)}.forall {n % _ != 0})
+
   // Stream.from implementation that returns a Stream[Long] rather than Stream[Int] (some of Project Euler's problems
   // require us to work with numbers that don't fit in an Int).
   private def fromLong(start: Long, step: Int): Stream[Long] = start #:: fromLong(start + step, step)
 
   // Stream of all prime numbers. I probably wouldn't do that in production code as it's a memory leak waiting to
   // happen, but it's nice for simple scripts.
+  // Note that we increment the counter by 2 because 2 is the only even prime number and it's already in the stream,
+  // so we can skip all other even numbers.
   val primes: Stream[Long] = 2 #:: fromLong(3, 2).filter {_.isPrime}
 }
--- src/scala/Problem003.scala
+++ src/scala/Problem003.scala
@@ -3,25 +3,12 @@
  * 
  * What is the largest prime factor of the number 600851475143 ?
  */
-def findMaxPrimeFactor(n: Long) = {
-  import EulerLong._
 
-  def findR(n: Long, primes: Stream[Long], result: List[Long]): List[Long] = {
-    // n isn't divisible anymore: we're done.
-    if(n.isPrime) n :: result
+// This problem is solved in our EulerLong class, in a relatively straightforward way: it computes the requested
+// number's prime factors and extracts the largest.
+// There might very well be a more clever approach, but this one has the advantage of being generic.
 
-    // n is still divisible, find its remaining prime factors.
-    else {
-      val m = primes.head
+import EulerLong._
 
-      // We're not using primes.tail in the recursion because nothing says that prime factors can't be duplicated.
-      if(n % m == 0) findR(n / m, primes, m :: result)
-      else findR(n, primes.tail, result)
-    }
-  }
-
-  findR(n, primes, Nil).max
-}
-
-EulerTimer {findMaxPrimeFactor(600851475143l)}
+EulerTimer {600851475143l.primeFactors.max}
 
